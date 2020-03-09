7efad7b3328f6b79158a286f8e75b1818142d230
Craig Condon <craig@captec.io>
2020-03-09 12:08:31 -0500

    gatsby init

---
+++ packages/paperclip-website/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+// import { Link } from "gatsby";
+import Home from "./home.pc";
+
+const IndexPage = () => (
+  <Home>something</Home>
+);
+
+export default IndexPage;
