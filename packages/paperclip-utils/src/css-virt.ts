8f91f202f643d3662139e8846c4d9760ca7db79f
Craig Condon <craig@captec.io>
2020-05-24 14:54:49 -0500

    properly extract

--- packages/paperclip-utils/src/css-virt.ts
+++ packages/paperclip-utils/src/css-virt.ts
@@ -8,42 +8,49 @@ export enum VirtRuleKind {
 }
 
 export type BaseVirtRule<TKind extends VirtRuleKind> = {
-  kind: TKind
+  kind: TKind;
 };
 
 export type VirtStyleRule = {
-  selector_text: string,
+  selector_text: string;
 } & BaseVirtRule<VirtRuleKind.Style>;
 
 export type MediaRule = {
-  rules: VirtRule[]
+  rules: VirtRule[];
 } & BaseVirtRule<VirtRuleKind.Media>;
 
 export type VirtRule = VirtStyleRule | MediaRule;
 
-export const getAllVirtSheetClassNames = (sheets: Record<string, VirtSheet>, allClassNames: string[] = []) => {
+export const getAllVirtSheetClassNames = (
+  sheets: Record<string, VirtSheet>,
+  allClassNames: string[] = []
+) => {
   for (const uri in sheets) {
     getAllVirtRuleClassNames(sheets[uri].rules, allClassNames);
   }
   return allClassNames;
 };
 
-const getAllVirtRuleClassNames = (rules: VirtRule[], allClassNames: string[]) => {
+const getAllVirtRuleClassNames = (
+  rules: VirtRule[],
+  allClassNames: string[]
+) => {
   for (const rule of rules) {
     getAllVirtRuleClassNames2(rule, allClassNames);
   }
   return allClassNames;
 };
 
-
 const getAllVirtRuleClassNames2 = (rule: VirtRule, allClassNames: string[]) => {
-  switch(rule.kind) {
+  switch (rule.kind) {
     case VirtRuleKind.Media: {
       getAllVirtRuleClassNames(rule.rules, allClassNames);
       break;
     }
     case VirtRuleKind.Style: {
-      const classNames = (rule.selector_text.match(/\.[^\s:[~>+]+/g) || []).map(str => str.substr(1))
+      const classNames = (
+        rule.selector_text.match(/\.[^\s:\[~>+,\.]+/g) || []
+      ).map(str => str.substr(1));
       for (const className of classNames) {
         if (allClassNames.indexOf(className) === -1) {
           allClassNames.push(className);
