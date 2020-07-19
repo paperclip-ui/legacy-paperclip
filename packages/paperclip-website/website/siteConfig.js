e36d4dab7f64634378bf6b1558b343a14d580b2e
Craig Condon <craig@captec.io>
2020-07-19 11:47:26 -0500

    change org

--- packages/paperclip-website/website/siteConfig.js
+++ packages/paperclip-website/website/siteConfig.js
@@ -36,7 +36,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: "Paperclip",
-  organizationName: "facebook",
+  organizationName: "crcn",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
