fd00cd18e8a17891e545dc6a9d0c2743c700b990
Craig Condon <craig@captec.io>
2020-07-19 11:35:26 -0500

    change base url

--- packages/paperclip-website/website/siteConfig.js
+++ packages/paperclip-website/website/siteConfig.js
@@ -24,10 +24,7 @@ const siteConfig = {
   title: "Paperclip", // Title for your website.
   tagline: "A website for testing",
   url: "https://paperclip.ev", // Your website URL
-  baseUrl: "/", // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  baseUrl: "https://github.com/crcn/paperclip",
 
   algolia: {
     apiKey: "24dd5424911cddbd61fa5057d8452c70",
