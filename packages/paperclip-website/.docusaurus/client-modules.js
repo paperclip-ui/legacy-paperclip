653f41e2eab6fb271a34ffae87575971ad7700d6
Craig Condon <craig@captec.io>
2020-07-21 09:06:19 -0500

    remove hidden dir

--- packages/paperclip-website/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/craigcondon/Developer/public/paperclip/packages/paperclip-website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/craigcondon/Developer/public/paperclip/packages/paperclip-website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/craigcondon/Developer/public/paperclip/packages/paperclip-website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/craigcondon/Developer/public/paperclip/packages/paperclip-website/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/craigcondon/Developer/public/paperclip/packages/paperclip-website/src/css/custom.css")
-];
