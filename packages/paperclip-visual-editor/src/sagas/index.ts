809419faef55acb22ff2330f9e2c5090b4a771d7
Craig Condon <craig@captec.io>
2020-08-18 21:09:25 -0500

    open link only when meta click

--- packages/paperclip-visual-editor/src/sagas/index.ts
+++ packages/paperclip-visual-editor/src/sagas/index.ts
@@ -139,7 +139,7 @@ const handleInteractionsForRenderer = (renderer: Renderer) =>
 
 function* handleCanvasElementClicked(action: CanvasElementClicked) {
   if (!action.payload.metaKey) {
-    // return;
+    return;
   }
 
   const state: AppState = yield select();
