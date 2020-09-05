f464370add125df40983f6b9ce69fe88f260d9dd
Craig Condon <craig@captec.io>
2020-09-04 21:20:20 -0500

    start on file navigator

---
+++ packages/paperclip-visual-editor/src/components/Editor/LeftSidebar/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as styles from "./index.pc";
+
+export const LeftSidebar = () => {
+  return (
+    <>
+      <styles.Container>CONTAINR;</styles.Container>
+    </>
+  );
+};
--- packages/paperclip-visual-editor/src/components/Editor/index.tsx
+++ packages/paperclip-visual-editor/src/components/Editor/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { withAppStore } from "../../hocs";
 import { Center } from "./Center";
+import { LeftSidebar } from "./LeftSidebar";
 import * as styles from "./index.pc";
+import { EMBEDDED } from "../../constants";
 
 export const Editor = withAppStore(() => {
   return (
     <styles.Container>
+      {EMBEDDED ? null : <LeftSidebar />}
       <Center />
     </styles.Container>
   );
