95e305498673f45aeb28de53c0093eaa549956ed
Craig Condon <craig@captec.io>
2021-01-26 19:58:33 +0800

    revert

--- packages/paperclip-visual-editor/src/contexts/index.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Dispatch } from "redux";
-import { AppState } from "../state";
-import { Action } from "../actions";
-
-export const AppStoreContext = React.createContext<{
-  state: AppState;
-  dispatch: Dispatch<Action>;
-}>(null);
--- packages/paperclip-visual-editor/src/hooks/useAppStore/index.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { AppStoreContext } from "../../contexts";
-
-export const useAppStore = () => {
-  return useContext(AppStoreContext);
-};
