682ad50c6de8f287989c0f3250e814205542fd8c
Craig Condon <craig@captec.io>
2021-02-10 10:14:05 +0800

    comment out

--- packages/paperclip/native/src/engine/mod.rs
+++ packages/paperclip/native/src/engine/mod.rs
@@ -152,9 +152,9 @@ impl Engine {
     self.listeners.push(listener);
   }
 
-  pub fn inspect_node(&mut self, node_path: Vec<i32>, document_uri: String) {
+  // pub fn inspect_node(&mut self, node_path: Vec<i32>, document_uri: String) {
     
-  }
+  // }
 
   fn dispatch(&self, event: EngineDelegateEvent) {
     for listener in &self.listeners {
