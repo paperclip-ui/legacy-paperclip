49676fb5a83cf279ced8a9d99e1695ab822ee30b
Craig Condon <craig.j.condon@gmail.com>
2021-03-02 11:43:42 +0800

    Update index.js

--- packages/paperclip-website/src/pages/index.js
+++ packages/paperclip-website/src/pages/index.js
@@ -93,7 +93,7 @@ function Home() {
               <>
                 <buttonStyles.Anchor
                   className={typography.classNames["semi-bold"]}
-                  href="https://ntudb0fdlly.typeform.com/to/V8Vur6fY"
+                  href="https://forms.gle/WJDVJEm9siYatABcA"
                   strong
                 >
                   Sign up for early access
