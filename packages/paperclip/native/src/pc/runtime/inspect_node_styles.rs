4bcb7181cec684b0053c32df2de69c76a05af5d1
Craig Condon <craig.j.condon@gmail.com>
2021-07-14 12:51:46 -0400

    start in inspection code

--- packages/paperclip/native/src/pc/runtime/inspect_node_styles.rs
+++ packages/paperclip/native/src/pc/runtime/inspect_node_styles.rs
@@ -1,6 +1,40 @@
+pub struct StyleDeclarationDetails {
+  pub name: String,
+
+  // TODO - should parse this out and provide AST here. For 
+  // MVP this is fine however.
+  pub value: String
+}
+
+
+pub struct StyleRuleDetails {
+
+  // computed style info. TODO: include scopes for this? 
+  selector_text: String,
+
+  // AST source information
+  source_id: String,
+  media: Option<String>,
+
+
+  declarations: Vec<StyleDeclarationDetails>
+}
+
+pub struct InspectionOptions {
+  screen_width: Option<i32>
+}
+
 pub fn inspect_node_styles(
   eval_info: &EvalInfo,
   graph: &DependencyGraph,
-  options: LintOptions,
+  options: &InspectionOptions
 ) -> Vec<LintWarning> {
+  /*
+  TODO:
+
+  - scan all style selectors loaded in eval info
+  - include triggers for media queries
+  - parse declaration values (AST code should be coming in from CSS)
+
+  */
 }
