f68b8d1335db4dc3383a2d9676b5addda028c62c
Craig Condon <craig.j.condon@gmail.com>
2021-12-12 17:02:08 -0500

    include global

--- packages/paperclip-cli/src/scaffolding/generators/root.ts
+++ packages/paperclip-cli/src/scaffolding/generators/root.ts
@@ -4,9 +4,8 @@ import * as path from "path";
 import { FILES_DIR } from "./utils";
 import { GeneratorKind } from "./base";
 import { prompt } from "inquirer";
-import { node } from "./node";
-import { percy } from "./percy";
 import { PaperclipConfig } from "paperclip";
+import * as shell from "shelljs";
 
 const HELLO_WORLD_CONTENT = fsa.readFileSync(
   path.join(FILES_DIR, "hello-paperclip.pc"),
@@ -16,53 +15,30 @@ const HELLO_WORLD_CONTENT = fsa.readFileSync(
 export const root = {
   kind: GeneratorKind.Root,
   async getParams({ cwd, isNewDirectory }) {
-    const { sourceDirectory } = await prompt([
+    const { sourceDirectory, compilerName } = await prompt([
       {
         name: "sourceDirectory",
         message: "Source directory where your *.pc files live",
         default: "./src"
+      },
+      {
+        name: "compilerName",
+        type: "list",
+        message: "Is there a compiler that you'd like to use?",
+        choices: [
+          { name: "None", value: null },
+          { name: "React", value: "paperclip-compiler-react" }
+        ]
       }
     ]);
-
-    let includePercy = false;
-    const overwrite = isNewDirectory;
-
-    // if (!isNewDirectory) {
-    //   overwrite = (
-    //     await prompt([
-    //       {
-    //         name: "overwrite",
-    //         message:
-    //           "Would you like to continue as a new project (some files may be overwritten)?",
-    //         type: "confirm",
-    //         default: false
-    //       }
-    //     ])
-    //   ).overwrite;
-    // } else {
-    //   overwrite = true;
-    // }
-
-    if (overwrite) {
-      includePercy = (
-        await prompt([
-          {
-            name: "includePercy",
-            type: "confirm",
-            message: "Use Percy for visual regression testing?",
-            default: false
-          }
-        ])
-      ).includePercy;
-    }
 
     return [
       {
         cwd,
         sourceDirectory,
-        overwrite
+        compilerName
       },
-      [node, ...(includePercy ? [percy] : [])]
+      []
     ];
   },
   prepare(params) {
@@ -75,17 +51,34 @@ export const root = {
       [GeneratorKind.Root]: params
     };
   },
-  generate(
-    { sourceDirectory: srcDir, overwrite, cwd }: any,
-    { [GeneratorKind.Root]: { compilerName } = { compilerName: null } }: any
-  ) {
+  async install({ compilerName, cwd }: any) {
+    if (compilerName) {
+      const npmClient = fsa.existsSync(path.join(cwd, "yarn.lock"))
+        ? "yarn"
+        : "npm";
+      const inPackage = fsa.existsSync(path.join(cwd, "package.json"));
+
+      let cmd = `npm i ${compilerName} -g`;
+
+      if (inPackage) {
+        if (npmClient === "yarn") {
+          cmd = `yarn add ${compilerName} --dev`;
+        } else {
+          cmd = `npm i ${compilerName} --save-dev`;
+        }
+      }
+
+      console.info(cmd);
+
+      await shell.exec(cmd);
+    }
+  },
+  async generate({ sourceDirectory: srcDir, cwd }: any) {
     const config: PaperclipConfig = {
       srcDir
     };
 
-    const files = {
-      [srcDir + "/hello-paperclip.pc"]: overwrite ? HELLO_WORLD_CONTENT : null
-    };
+    const files = {};
 
     if (!fsa.existsSync(path.join(cwd, "paperclip.config.json"))) {
       files["paperclip.config.json"] = JSON.stringify(config, null, 2);
