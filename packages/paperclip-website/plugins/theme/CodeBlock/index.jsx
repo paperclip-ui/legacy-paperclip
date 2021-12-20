b6dee722db82b74ccdd204979fa5b7f2e8ae8cc9
Craig Condon <craig.j.condon@gmail.com>
2021-12-19 23:05:48 -0500

    update readme

--- packages/paperclip-website/plugins/theme/CodeBlock/index.jsx
+++ packages/paperclip-website/plugins/theme/CodeBlock/index.jsx
@@ -16,36 +16,6 @@ export default props => {
       </LiveEditor>
     );
   }
-  // if (props.live) {
-  //   const content = String(props.children);
-  //   const files = content.split(/\/\/\s*file:\s*/g).filter(Boolean);
-  //   const graph = {};
-
-  //   let entry;
-
-  //   for (const file of files) {
-  //     const name = (file.match(/(.*?\.pc)/) || [, "main.pc"])[1];
-  //     if (!entry) {
-  //       entry = name;
-  //     }
-
-  //     const content = file.replace(name, "").trim();
-  //     graph[name] = content;
-  //   }
-
-  //   return (
-  //     <>
-  //       <Editor
-  //         graph={graph}
-  //         previewStyle={{
-  //           height: props.height
-  //         }}
-  //         defaultUri={entry}
-  //         theme={prismTheme}
-  //       />
-  //     </>
-  //   );
-  // }
 
   return <CodeBlock {...props} />;
 };
