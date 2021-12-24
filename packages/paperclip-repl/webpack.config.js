0870568dbe1da796ddb2589b744d789e04b28c39
Craig Condon <craig.j.condon@gmail.com>
2021-12-24 12:10:31 -0500

    start on repl

---
+++ packages/paperclip-repl/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/entry.tsx",
+
+  output: {
+    filename: "[name]-[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      publicPath: "/",
+      title: "React Demo",
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+        exclude: []
+      },
+      {
+        test: /\.pc$/,
+        loader: "paperclip-loader",
+        include: [path.resolve(__dirname, "src")],
+        options: {
+          config: require("./paperclip.config.json")
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif|ttf|svg)$/i,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
