551a07b6c65138e3950ac77e89ce50fed86f1988
Craig Condon <craig.j.condon@gmail.com>
2022-01-03 12:07:12 -0500

    fix tsc

--- packages/paperclip-compiler-html/src/index.ts
+++ packages/paperclip-compiler-html/src/index.ts
@@ -78,6 +78,7 @@ export const compile = compilers({
     vendorName: "_vanilla"
   },
   definition: {
+    imports: "",
     elementType: "string"
   },
   extensionName: (config: PaperclipConfig) =>
