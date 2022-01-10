c4d631221df5daa8f083d285ab3da1ed097dbc73
Craig Condon <craig.j.condon@gmail.com>
2022-01-10 13:30:03 -0500

    fix jest compiler

--- packages/jest-paperclip/src/index.ts
+++ packages/jest-paperclip/src/index.ts
@@ -1,6 +1,8 @@
 import * as url from "url";
 import * as fs from "fs";
 import * as babel from "@babel/core";
+import * as path from "path";
+import { buildCompilerOptions } from "@paperclip-ui/utils";
 import {
   createEngineDelegate,
   PC_CONFIG_FILE_NAME,
@@ -22,9 +24,30 @@ module.exports = {
       fs.readFileSync(url.fileURLToPath(pcUrl), "utf8")
     );
 
+    let code;
+
+    // somewhat of a dumb approach to figuring out what emits. JS will want to be more specific about this at
+    // some point. Maybe pulling in env var as a good stop gap?
+    for (const targetCompilerOptions of buildCompilerOptions(config)) {
+      if (
+        targetCompilerOptions.emit &&
+        !targetCompilerOptions.emit.includes("js")
+      ) {
+        continue;
+      }
+      code = buildFile(fileUri, engine, {
+        cwd: path.dirname(fullPath),
+        config,
+        targetCompilerOptions
+      }).translations[".js"];
+
+      if (code) {
+        break;
+      }
+    }
+
     engine.open(fileUri);
-    const { js } = buildFile(fileUri, engine, { config }).translations[".js"];
-    return babel.transformSync(js, {
+    return babel.transformSync(code, {
       presets: ["@babel/env"],
       configFile: false
     }).code;
