0de3c47a0861ca430568e59f0ed359080d97eefc
Craig Condon <craig.j.condon@gmail.com>
2022-01-20 22:38:57 -0500

    move mock to separate file

--- packages/tandem-designer/src/__TEST__/basic.test.tsx
+++ packages/tandem-designer/src/__TEST__/basic.test.tsx
@@ -1,61 +1,36 @@
-import { createMemoryHistory } from "history";
-import { Store } from "redux";
-import * as URL from "url";
-import { createTestServer } from "@tandem-ui/workspace/src/test/utils";
-import { createAppStore } from "../components/Main/create-app-store";
 import { canvasResized, workspaceActions } from "..";
-import { middlewareSpy } from "./utils";
+import { createMock, DesignerMock } from "./utils";
 import { AppState } from "../state";
 
 describe(`With a basic project`, () => {
-  let testServer;
-  let store;
-  let waitForAction;
+  let mock: DesignerMock;
 
   beforeEach(async () => {
-    testServer = await createTestServer({
-      "test.pc": "Hello world",
-    });
-
-    const client = testServer.createClient();
-    const project = await client.openProject({
-      uri: URL.pathToFileURL(testServer.testDir).href,
-    });
-    const history = createMemoryHistory();
-    history.push(
-      `?projectId=${project.getProperties().id}&canvasFile=${encodeURIComponent(
-        testServer.fixtureUris["test.pc"]
-      )}`
-    );
-
-    const { waitForAction: waitForAction2, middleware } = middlewareSpy();
-    waitForAction = waitForAction2;
-    store = createAppStore({
-      history,
-      middleware,
-      createRPCClient() {
-        return testServer.createConnection();
+    mock = await createMock({
+      files: {
+        "test.pc": "Hello world",
       },
+      canvasFile: "test.pc",
     });
   });
 
   afterEach(() => {
-    testServer.stop();
+    mock.dispose();
   });
 
   test(`Automatically loads all frame on init`, async () => {
-    const frames = (await waitForAction(
+    const frames = (await mock.waitForAction(
       workspaceActions.allFramesLoaded.type
     )) as ReturnType<typeof workspaceActions.allFramesLoaded>;
   });
 
   test(`Automatically centers the canvas when a canvas is loaded`, async () => {
-    store.dispatch(canvasResized({ width: 3000, height: 3000 }));
-    let state = store.getState() as AppState;
-    (await waitForAction(workspaceActions.allFramesLoaded.type)) as ReturnType<
-      typeof workspaceActions.allFramesLoaded
-    >;
-    state = store.getState() as AppState;
+    mock.store.dispatch(canvasResized({ width: 3000, height: 3000 }));
+    let state = mock.store.getState() as AppState;
+    (await mock.waitForAction(
+      workspaceActions.allFramesLoaded.type
+    )) as ReturnType<typeof workspaceActions.allFramesLoaded>;
+    state = mock.store.getState() as AppState;
     expect(state.designer.canvas.transform).toEqual({ x: 988, y: 1116, z: 1 });
   });
 });
--- packages/tandem-designer/src/__TEST__/utils.ts
+++ packages/tandem-designer/src/__TEST__/utils.ts
@@ -1,10 +1,17 @@
+import * as URL from "url";
+import { createTestServer } from "@tandem-ui/workspace/src/test/utils";
+import { createMemoryHistory } from "history";
+import { createAppStore } from "../components/Main/create-app-store";
+import { Store } from "redux";
+import { Action } from "..";
+
 export const middlewareSpy = () => {
   let waitType;
   let resolveAction;
 
   const waitForAction = (type: string) => {
     waitType = type;
-    return new Promise((resolve) => {
+    return new Promise<Action>((resolve) => {
       resolveAction = resolve;
     });
   };
@@ -20,4 +27,49 @@ export const middlewareSpy = () => {
     middleware,
     waitForAction,
   };
+};
+
+type CreateMockOptions = {
+  files: Record<string, string>;
+  canvasFile: string;
+};
+
+export type DesignerMock = {
+  store: ReturnType<typeof createAppStore>;
+  waitForAction: (type: string) => Promise<Action>;
+  dispose: () => void;
+};
+
+export const createMock = async ({
+  files,
+  canvasFile,
+}: CreateMockOptions): Promise<DesignerMock> => {
+  const testServer = await createTestServer(files);
+
+  const client = testServer.createClient();
+  const project = await client.openProject({
+    uri: URL.pathToFileURL(testServer.testDir).href,
+  });
+  const history = createMemoryHistory();
+  history.push(
+    `?projectId=${project.getProperties().id}&canvasFile=${encodeURIComponent(
+      testServer.fixtureUris[canvasFile]
+    )}`
+  );
+
+  const { waitForAction, middleware } = middlewareSpy();
+
+  const store = createAppStore({
+    history,
+    middleware,
+    createRPCClient() {
+      return testServer.createConnection();
+    },
+  });
+
+  const dispose = () => {
+    testServer.stop();
+  };
+
+  return { store, waitForAction, dispose };
 };
